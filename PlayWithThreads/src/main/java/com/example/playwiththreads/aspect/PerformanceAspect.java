bfce9bca0b7b402c5944311cd7ab519d96310c5c
Ahmet <AhmetTK4@users.noreply.github.com>
2024-10-22 23:47:44 +0300

    Add PerformanceAspect clas for logging

---
+++ PlayWithThreads/src/main/java/com/example/playwiththreads/aspect/PerformanceAspect.java
@@ -0,0 +1,22 @@
+package com.example.playwiththreads.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class PerformanceAspect {
+
+    @Around("@annotation(org.springframework.scheduling.annotation.Scheduled)")
+    public Object measureExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long end = System.currentTimeMillis();
+        log.info("{} metodu {} ms sürdü.", joinPoint.getSignature(), (end - start));
+        return result;
+    }
+}
