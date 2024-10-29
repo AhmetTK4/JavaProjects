f83617ad2c8b20df9c1dbaeb16c9cff14c07367b
Ahmet <AhmetTK4@users.noreply.github.com>
2024-10-29 22:33:20 +0300

    New build

---
+++ ProxyDesignPattern/src/main/java/com/example/proxydesignpattern/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.proxydesignpattern.service;
+
+import com.example.proxydesignpattern.entity.User;
+import com.example.proxydesignpattern.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public void updateUserEmail(Long id, String newEmail) {
+        userRepository.findById(id).ifPresentOrElse(user -> {
+            user.setEmail(newEmail);
+            userRepository.save(user);
+        }, () -> {
+            throw new RuntimeException("User not found");
+        });
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
