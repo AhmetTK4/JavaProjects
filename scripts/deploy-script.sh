a7dba493667d6b74aa3bf81ff842152781e457de
Ahmet <69666756+AhmetTK4@users.noreply.github.com>
2024-10-29 16:47:26 +0300

    Create deploy-script.sh

---
+++ scripts/deploy-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting deployment process..."
+
+
+echo "Simulating copying files to /opt/x-app..."
+sleep 1 
+
+
+echo "Simulating application start..."
+sleep 1 
+
+echo "Fake deployment completed successfully!"
