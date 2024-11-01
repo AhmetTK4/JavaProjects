5ac64f5ba81fb5a98a451b7a4a3b68289febd232
Ahmet <AhmetTK4@users.noreply.github.com>
2024-11-02 00:06:49 +0300

    New Build

---
+++ PlayWithCaches/src/main/java/com/example/playwithcaches/PlayWithCachesApplication.java
@@ -0,0 +1,13 @@
+package com.example.playwithcaches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWithCachesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayWithCachesApplication.class, args);
+    }
+
+}
